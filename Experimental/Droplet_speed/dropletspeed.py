ec22099a046d753f51e82660ac1b15be9aba18e5
Kenza Samlali <kenzasamlali@gmail.com>
2021-02-11 16:03:48 -0500

    adding metadata

---
+++ Experimental/Droplet_speed/dropletspeed.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+#filename='20210210-dropletspeed-dgw003-dgo004-sp00-00003.txt'
+mypath='E:/KENZA Folder/PROJECT2_ClRosea/sorting/droplet_HS/metadata/'
+start= [6, 2, 5, 5, 5, 3, 7, 3, 4, 7, 7, 7, 8, 10, 26, 20, 8, 7, 12, 2, 26, 6, 7, 23, 24, 15, 39, 3, 4, 6, 35, 17, 27, 1, 17, 29, 23, 17, 8, 7, 25, 13 ]
+#stop= [36, 27, 28, 27, 25, 22, 25, 19, 19, 21, 21, 19, 20, 21, 37, 30, 19, 17, 21 , 11, 34, 14, 14, 30, 30, 21, 45, 8, 9, 11, 40, 22, 31, 5, 18/2, 33/2, 25, 19, 10, 9, 27, 15 ]
+stop= [36, 27, 28, 27, 25, 22, 25, 19, 19, 21, 21, 19, 20, 21, 37, 30, 19, 17, 21 , 11, 34, 14, 14, 30, 30, 21, 45, 8, 9, 11, 40, 22, 31, 5, 18, 33, 25, 19, 10, 9, 27, 15 ]
+
+def returnspeed(filename,first_frame, last_frame):
+    df = pd.read_csv(str(filename), header=0, sep="\t")
+    #print df
+    new_df = df[df['Key'].str.contains('Time_From_Last')]
+    new_df.head()
+    #print new_df
+    df3=df[df['Key'].str.match('Field %d Time_From_Last'%(first_frame))]
+    for i in range(first_frame+1,last_frame+1):
+        df2 = df[df['Key'].str.match('Field %d Time_From_Last'%(i))]
+        thing =[df3, df2]
+        df3=pd.concat(thing)
+    #print df3
+    df3.head()
+    df3['Value']=pd.to_numeric(df3['Value'])
+    totaltime=df3['Value'].sum()
+    return totaltime
+
+#list all flowrates
+files = [mypath+f for f in listdir(mypath) if isfile(join(mypath, f))]
+flowrates = [f[-7:-4] for f in listdir(mypath) if isfile(join(mypath, f))]
+#print flowrates
+
+data = {'File': files, 
+'Flowrate': flowrates, 
+'start': start,
+'stop': stop
+}
+df = pd.DataFrame(data, columns=['File','Flowrate', 'start', 'stop'])
+print df
+df['Speed'] = df.apply(lambda x: returnspeed(x['File'], x['start'], x['stop']), axis=1) 
+print (df)
--- Experimental/dropletspeed.R
+++
@@ -1,8 +0,0 @@
-for 
-#oilspeed, [start, end]
-frames = {'00005': [5, 25]
-
-}
-
-for i in range(6,25):
-Field i Time_From_Last
