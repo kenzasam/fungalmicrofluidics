c4b5550054cd8c9baabd3c0302346358848ebc39
Kenza Samlali <kenzasamlali@gmail.com>
2021-02-16 10:36:03 -0500

    capstone

--- capstone_acquisition.py
+++ capstone_acquisition.py
@@ -2,6 +2,8 @@ from pycromanager import Acquisition, multi_d_acquisition_events, Bridge, Datase
 from matplotlib import pyplot as plt
 import numpy as np
 import time
+import os, sys
+from PIL import Image
 
 bridge = Bridge()
 core = bridge.get_core()
@@ -32,13 +34,77 @@ z_array = np.array(z_array)
 
 
 with Acquisition(directory='E:\KENZA Folder\CapstoneTests', name='saving_name') as acq:
-      #Generate the events for a single z-stack
-      xyz = np.hstack([x_array[:, None], y_array[:, None], z_array[:, None]])
-      events = multi_d_acquisition_events(xyz_positions=xyz)
-      acq.acquire(events)
+   x=np.hstack([x_array[:, None]])
+   y=np.hstack([y_array[:, None]])
+   z=np.hstack([z_array[:, None]])
+   #Generate the events for a single z-stack
+   xyz = np.hstack([x_array[:, None], y_array[:, None], z_array[:, None]])
+   events = multi_d_acquisition_events(xyz_positions=xyz)           
+   acq.acquire(events)
+   #acquire a 2 x 1 grid
+   #acq.acquire({'row': 0, 'col': 0})
+   #acq.acquire({'row': 1, 'col': 0})
+      
 
-#dataset = acq.get_dataset().as_array(stitched=True)
-dataset = acq.get_dataset()
-img = dataset.read_image() # <---- read our z-stack index 0
-plt.imshow(img)  # <---- convert numpy array into image
+#data_path ='E:\KENZA Folder\CapstoneTests\saving_name_18'
+#dataset = Dataset(data_path)
+dataset2 = acq.get_dataset()
+#dataset = acq.get_dataset()
+
+length=(len(xyz))
+
+def merge_images(file1, file2):
+   """Merge two images into one vertical image
+   :param file1: path to first image file
+   :param file2: path to second image file
+   :return: the merged Image object
+   """
+   image1 = file1
+   image2 = file2
+ 
+   (width1, height1) = image1.size
+   (width2, height2) = image2.size
+ 
+   # result_width = width1 + width2
+   result_width = width1
+   # result_height = max(height1, height2)
+   result_height = height1 + height2
+ 
+   print (height2)
+ 
+   result = Image.new('RGB', (result_width, result_height))
+   result.paste(im=image1, box=(0, 0))
+   result.paste(im=image2, box=(0, height1))
+   return result
+
+stitched_img = dataset2.read_image(position=0)
+img_array=[]
+
+for x_pos in range(length): 
+   print(x_pos)
+  #for y_pos in range(length):
+      #print(dataset2.has_image(row=x_pos, col=y_pos))
+   img = dataset2.read_image(position=x_pos) # <---- read our z-stack index 0
+   #plt.imshow(img)  # <---- convert numpy array into image
+   #plt.show()  # <--- show us the image
+   
+   img_array.append(img)
+   #stiched_img = merge_images(stitched_img, img)
+
+# creates a new empty image, RGB mode, and size 444 by 95
+new_im = Image.new('RGB', (444,95))
+
+for elem in img_array:
+    for i in xrange(0,444,95):
+        im=Image.open(elem)
+        new_im.paste(im, (i,0))
+new_im.save('test.jpg')
+
+plt.imshow(stitched_img)  # <---- convert numpy array into image
 plt.show()  # <--- show us the image
+
+
+#img = dataset2.read_image(row=0, col=0) # <---- read our z-stack index 0
+#plt.imshow(img)  # <---- convert numpy array into image
+#plt.show()  # <--- show us the image
+
