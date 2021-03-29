dab99e9fe3d1b6d6c458fe53041dabb32e0331eb
Kenza Samlali <kenzasamlali@gmail.com>
2021-03-29 17:30:45 -0400

    bridge

--- ArduBridge_ClR.py
+++ ArduBridge_ClR.py
@@ -81,8 +81,8 @@ if __name__ == "__main__":
     PID = True #<-- True / False to build a PID controller.
     MM_PROC = False #<-- True / False to access micro manager core and perform image processing.
     PUMPS = False #<-- True when user wants to use Nemesys pump through python.
-    SPEC = True #<-- True when user wants to use a spectrometer thread.
-    SPECSP = True #<-- True when user wants to perform signal processing on spectrum .
+    SPEC = False #<-- True when user wants to use a spectrometer thread.
+    SPECSP = False #<-- True when user wants to perform signal processing on spectrum .
     GUI = False #<-- True for running GUI through serial
     STACK_BUILD = [0x40,0x41,0x42,0x43,0x44,0x45] #<-- Adresses for port expanders on optocoupler stack
     PORT_BASE = 7000
